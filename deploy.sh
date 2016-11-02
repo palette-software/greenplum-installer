8cb19dbcc4fab66097d01168a581b51a13bf1c82
Peter Bastyi <baspeti@gmail.com>
2016-11-02 13:09:45 +0100

    Do not attempt to deploy to Github Releases

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,8 @@ set -e
 # Upload the RPM to the RPM repository
 # by exportin it to SSHPASS, sshpass wont log the command line and the password
 export SSHPASS=$DEPLOY_PASS
-sshpass -e scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r rpm-build/_build/* $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+sshpass -e scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r /tmp/gp-rpm-build/_build/x86_64/* $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 
 # Update the RPM repository
 export DEPLOY_CMD="createrepo ${DEPLOY_PATH}/"
 sshpass -e ssh  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST $DEPLOY_CMD
-
-./release-to-github.sh
